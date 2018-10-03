6afdb5f3991553572a0a9f7099e3b0eaea5c31b2
RayBowman2017b <RayBowman2017b@gmail.com>
2018-10-02 20:19:14 -0500

    2018-10-02-2022

--- z16gitpush.bat
+++ z16gitpush.bat
@@ -1,3 +1,4 @@
 @rem z16gitpush.bat
 @rem
 git push -u origin_02 master
+pause
---
+++ z32gitpush.bat
@@ -0,0 +1,5 @@
+@rem z32gitpush.bat
+@rem 
+@rem git remote add Budget_app_03 git@github.com:RayBowman2017b/react-course-2-budget-app_03.git
+git push -u Budget_app_03 master
+pause
